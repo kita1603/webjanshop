0dd59c31e3cd66c77d11c046484d4bc1697856d1
Le Phan Nhat Thien [jan] <jan@action2quare.com>
2024-04-09 17:14:21 +0700

    Add api: CurrentUserProfile, UpdateCurrentUserProfile. Add admin api: DeleteUser, GetUserById, UpdateUser

--- backend/routes/useRoutes.js
+++ backend/routes/useRoutes.js
@@ -1,5 +1,15 @@
 import express from "express";
-import { createUser, loginUser, logoutCurrentUser, getAllUsers } from "../controllers/useController.js";
+import { 
+    createUser, 
+    loginUser, 
+    logoutCurrentUser, 
+    getAllUsers,
+    getCurrentUserProfile,
+    updateCurrentUserProfile,
+    deleteUserById,
+    getUserById,
+    updateUserById,
+ } from "../controllers/useController.js";
 import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware.js";
 
 
@@ -10,6 +20,14 @@ router.route("/").post(createUser).get(authenticate, authorizedAdmin, getAllUser
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
 
+router.route("/profile").get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
+
+//admin routes
+router.route("/:id")
+.delete(authenticate, authorizedAdmin, deleteUserById)
+.get(authenticate, authorizedAdmin, getUserById)
+.put(authenticate, authorizedAdmin, updateUserById)
+;
 
 
 export default router;
