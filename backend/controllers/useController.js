d0712842ed28c758ba412adb83e82381fba456b5
Le Phan Nhat Thien [jan] <jan@action2quare.com>
2024-04-09 15:33:10 +0700

    add admin api: GetAllUser

--- backend/controllers/useController.js
+++ backend/controllers/useController.js
@@ -74,4 +74,9 @@ const logoutCurrentUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: "logged out successfully"});
 });
 
-export {createUser, loginUser, logoutCurrentUser};
+const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+});
+
+export {createUser, loginUser, logoutCurrentUser, getAllUsers};
--- backend/routes/useRoutes.js
+++ backend/routes/useRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createUser, loginUser, logoutCurrentUser } from "../controllers/useController.js";
+import { createUser, loginUser, logoutCurrentUser, getAllUsers } from "../controllers/useController.js";
+import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware.js";
+
+
 const router = express.Router();
 
 
-router.route("/").post(createUser);
+router.route("/").post(createUser).get(authenticate, authorizedAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
+
+
 
 export default router;
