7e35cc66059b0d7304f0ea618b6901495c22cbc1
kita1603 <nhatthienlephan@gmail.com>
2024-04-11 00:33:53 +0700

    frontend: add register page + connect api: register, logout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,9 @@ import {Route, RouterProvider, createRoutesFromElements} from "react-router";
 import { createBrowserRouter } from 'react-router-dom';
 import {Provider} from "react-redux";
 import store from './redux/store.js';
+
 import Login from './pages/Auth/Login.jsx';
+import Register from './pages/Auth/Register.jsx';
 
 
 
@@ -14,6 +16,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
     </Route>
   )
 )
