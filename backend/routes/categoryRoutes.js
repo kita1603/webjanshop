6a831d1217cd75f84a01b6a21c38f8f9474d79bb
kita1603 <nhatthienlephan@gmail.com>
2024-04-11 22:27:46 +0700

    backend: add api category (get, getall, update, delete)

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -2,18 +2,22 @@ import express from "express";
 const router = express.Router();
 import {
   createCategory,
+  updateCategory,
+  removeCategory,
+  listCategory,
+  readCategory,
 
 } from "../controllers/categoryController.js";
 
 import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware.js";
 
 router.route("/").post(authenticate, authorizedAdmin, createCategory);
-// router.route("/:categoryId").put(authenticate, authorizeAdmin, updateCategory);
-// router
-//   .route("/:categoryId")
-//   .delete(authenticate, authorizeAdmin, removeCategory);
+router.route("/:categoryId").put(authenticate, authorizedAdmin, updateCategory);
+router
+  .route("/:categoryId")
+  .delete(authenticate, authorizedAdmin, removeCategory);
 
-// router.route("/categories").get(listCategory);
-// router.route("/:id").get(readCategory);
+router.route("/categories").get(listCategory);
+router.route("/:id").get(readCategory);
 
 export default router;
